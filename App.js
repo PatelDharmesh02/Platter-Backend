02559a9da8b8d03bf39677c746be8e0c960bada1
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-10-12 16:00:56 +0530

    Feat: Added DB connection for restaurants data

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const { RESTAURANTS_DATA, RESTAURANTS_DETAILS } = require("./data");
+const mongoose = require("mongoose");
+const { RESTAURANTS_DETAILS } = require("./data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/restaurants", (req, res) => {
-  res.send(RESTAURANTS_DATA);
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await mongoose.connection.db
+      .collection("restaurants")
+      .find()
+      .toArray();
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).send("Error fetching restaurants: " + error.message);
+  }
 });
 
 app.get("/restaurant/:id", (req, res) => {
@@ -16,6 +29,13 @@ app.get("/restaurant/:id", (req, res) => {
   res.send(restaurantDetails);
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server started on port " + process.env.PORT || 5000);
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB" + err.message);
+  });
